6ab15e06a03cbcb3a79c3e2d398c35f3f766b490
Daniel Schlachter <schlachter.daniel@gmail.com>
2020-07-20 11:39:15 +0000

    improve controller, add comments

--- reassignUI/webapp/controller/ReassignView.controller.js
+++ reassignUI/webapp/controller/ReassignView.controller.js
@@ -4,7 +4,7 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("reassign.reassignUI.controller.ReassignView", {
-		_startInstance: function () {
+		_getTaskInstances: function () {
 			var model = this.getView().getModel();
 			$.ajax({
 				url: "/bpmworkflowruntime/workflow-service/rest/v1/task-instances?status=READY&status=RESERVED",
@@ -14,19 +14,21 @@ sap.ui.define([
 
 				success: function (result, xhr, data) {
 					model.setProperty("/result", result);
-					debugger;
 				}
 			});
 		},
 
 		onInit: function () {
 			this.getView().setModel(new sap.ui.model.json.JSONModel());
-			this._startInstance();
-
+			this._getTaskInstances();
 		},
 		
 		reassign: function (instanceId) {
-			var owner = this.getView().byId("processorInput").getValue()
+            //find the value entered as new processor in the input field
+            var owner = this.getView().byId("processorInput").getValue();
+            var that = this;
+
+            // update the workflow instance with the new processor
 			$.ajax({
 				url: "/bpmworkflowruntime/workflow-service/rest/v1/task-instances/" + instanceId,
 				method: "PATCH",
@@ -36,6 +38,8 @@ sap.ui.define([
 					"processor" : owner
 				}),
 				success: function (result, xhr, data) {
+                    //reload UI when task is reassigned successfully, so result becomes visible
+                    that._getTaskInstances();
 				}
 			});			
 
